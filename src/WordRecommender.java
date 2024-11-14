82d6111786399e7f4a4190801615b09aeaa40b4e
abby <abbyzhou@seas.upenn.edu>
2024-11-13 20:25:04 -0500

    before getSimilarity

--- src/WordRecommender.java
+++ src/WordRecommender.java
@@ -1,17 +1,55 @@
-public class WordRecommender {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordRecommender {
+    private ArrayList<String> dictionary = new ArrayList<>();
     public WordRecommender(String dictionaryFile) {    
       // TODO: change this!
+        try (FileInputStream inputStream = new FileInputStream(dictionaryFile);
+             Scanner fileScanner = new Scanner(inputStream)) {
+            while (fileScanner.hasNextLine()) {
+                String word = fileScanner.nextLine().trim().toLowerCase();
+                if (!dictionary.contains(word)) {
+                    dictionary.add(word);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.printf(Util.FILE_OPENING_ERROR);
+        } catch (IOException e) {
+            System.out.printf(Util.FILE_OPENING_ERROR);
+        }
     }
   
     public double getSimilarity(String word1, String word2) {
       // TODO: change this!
-      return 0.0;
+        int commonLetters = 0;
+        int maxLen = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < Math.min(word1.length(), word2.length()); i++) {
+            if (word1.charAt(i) == word2.charAt(i)) {
+                commonLetters++;
+            }
+        }
+        return (double) commonLetters / maxLen;
     }
   
     public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
       // TODO: change this!
-      return null;
+        ArrayList<String> suggestions = new ArrayList<>();
+
+        for (String dictWord : dictionary) {
+            double similarity = getSimilarity(word, dictWord);
+            if (similarity >= commonPercent) {
+                suggestions.add(dictWord);
+                if (suggestions.size() >= topN) {
+                    break;  // Limit to top N suggestions
+                }
+            }
+        }
+        return suggestions;
     }
   
     
