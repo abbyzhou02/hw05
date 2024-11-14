51c28fe61a2d8a55cbf40e5f557abb695791e2a1
abby <abbyzhou@seas.upenn.edu>
2024-11-13 23:28:35 -0500

    getReplacement

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,87 +1,109 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SpellChecker {
-    private static ArrayList<String> dictionary = new ArrayList<>();
+
+    private WordRecommender wordRecommender;
 
     public SpellChecker() {
-      // TODO: You can modify the body of this constructor,
-      // or you can leave it blank. You must keep the signature, however.
+        Scanner scanner = new Scanner(System.in);
+        File dictionaryFile = getFile(scanner, Util.DICTIONARY_PROMPT);
+        this.wordRecommender = new WordRecommender(dictionaryFile.getAbsolutePath());
     }
-  
-    public static void start() {
-      // TODO: You can modify the body of this method,
-      // or you can leave it blank. You must keep the signature, however.
-        Scanner scanner = new Scanner (System.in);
 
-        try {
-            String dictionaryFileName = promptForDictionary(scanner);
-            System.out.printf(Util.FILENAME_PROMPT);
-            String textFileName = scanner.nextLine();
-            checkSpelling(textFileName);
-        } catch (IOException e) {
-            System.out.printf(Util.FILE_OPENING_ERROR);
-        } finally {
-            scanner.close();
+    public SpellChecker(String dictionaryFile) {
+        this.wordRecommender = new WordRecommender(dictionaryFile);
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        File fileToCheck = getFile(scanner, Util.FILENAME_PROMPT);
+        if (fileToCheck == null) return;
+
+        String outputFileName = fileToCheck.getName().replace(".txt", "_chk.txt");
+
+        try (PrintWriter writer = new PrintWriter(outputFileName)) {
+            Scanner fileScanner = new Scanner(fileToCheck);
+
+            while (fileScanner.hasNext()) {
+                String word = fileScanner.next();
+
+                if (!wordRecommender.isWordInDictionary(word)) {
+                    ArrayList<String> suggestions = wordRecommender.getWordSuggestions(word, 2, 0.5, 4);
+                    String replacement = getReplacement(scanner, word, suggestions);
+                    writer.print(replacement + " ");
+                } else {
+                    writer.print(word + " ");
+                }
+            }
+
+            fileScanner.close();
+            System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, fileToCheck.getName(), outputFileName);
+        } catch (FileNotFoundException e) {
+            System.out.printf("Error in writing output file.");
         }
     }
 
-    private static String promptForDictionary(Scanner scanner) throws FileNotFoundException {
+    private File getFile(Scanner scanner, String prompt) {
         while (true) {
-            System.out.printf(Util.DICTIONARY_PROMPT);
-            String fileName = scanner.nextLine();
-
-            FileInputStream inputStream = new FileInputStream(fileName);
-            System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, fileName);
-            try {
-                loadDictionary(inputStream);
-            } catch (IOException e) {
+            System.out.printf(prompt);
+            String filename = scanner.nextLine();
+            File file = new File(filename);
+            if (file.exists() && !file.isDirectory()) {
+                System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, filename);
+                return file;
+            } else {
                 System.out.printf(Util.FILE_OPENING_ERROR);
-            } finally {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    System.out.println("Failed to close the file stream.");
+            }
+        }
+    }
+
+    private String getReplacement(Scanner scanner, String word, ArrayList<String> suggestions) {
+        System.out.printf(Util.MISSPELL_NOTIFICATION, word);
+
+        if (suggestions.isEmpty()) {
+            System.out.printf(Util.NO_SUGGESTIONS);
+            System.out.printf(Util.TWO_OPTION_PROMPT);
+            while (true) {
+                String option = scanner.nextLine();
+                if (option.equals("a")) {
+                    return word;
+                } else if (option.equals("t")) {
+                    System.out.printf(Util.MANUAL_REPLACEMENT_PROMPT);
+                    return scanner.nextLine();
+                } else {
+                    System.out.printf(Util.INVALID_RESPONSE);
                 }
             }
-            return fileName;
-        }
-    }
-
-    private static void loadDictionary(FileInputStream inputStream) throws IOException {
-        Scanner fileScanner = new Scanner(inputStream);
-        while (fileScanner.hasNextLine()) {
-            String word = fileScanner.nextLine().trim().toLowerCase();
-            if (!dictionary.contains(word)) {
-                dictionary.add(word);
+        } else {
+            System.out.printf(Util.FOLLOWING_SUGGESTIONS);
+            for (int i = 0; i < suggestions.size(); i++) {
+                System.out.printf(Util.SUGGESTION_ENTRY, i + 1, suggestions.get(i));
             }
-        }
-        fileScanner.close();
-    }
-    private static void checkSpelling(String fileName) throws IOException {
-        FileInputStream inputStream = new FileInputStream(fileName);
-        Scanner fileScanner = new Scanner(inputStream);
+            System.out.printf(Util.THREE_OPTION_PROMPT);
 
-        System.out.println("Misspelled words:");
-        while (fileScanner.hasNextLine()) {
-            String[] words = fileScanner.nextLine().toLowerCase().split(" ");
-            for (String word : words) {
-                if (!dictionary.contains(word)) {
-                    System.out.printf(Util.MISSPELL_NOTIFICATION, word);
+            while (true) {
+                String option = scanner.nextLine();
+                if (option.equals("r")) {
+                    System.out.printf(Util.AUTOMATIC_REPLACEMENT_PROMPT);
+                    int choice = Integer.parseInt(scanner.nextLine());
+                    if (choice > 0 && choice <= suggestions.size()) {
+                        return suggestions.get(choice - 1);
+                    } else {
+                        System.out.printf(Util.INVALID_RESPONSE);
+                    }
+                } else if (option.equals("a")) {
+                    return word;
+                } else if (option.equals("t")) {
+                    System.out.printf(Util.MANUAL_REPLACEMENT_PROMPT);
+                    return scanner.nextLine();
+                } else {
+                    System.out.printf(Util.INVALID_RESPONSE);
                 }
             }
         }
-
-        fileScanner.close();
-        inputStream.close();
     }
-
-    public static boolean isWordInDictionary(String word) {
-        return dictionary.contains(word);
-    }
-    // You can of course write other methods as well.
-  }
+}
